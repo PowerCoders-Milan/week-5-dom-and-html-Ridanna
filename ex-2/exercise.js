5e1b7779a05797cde12821a84051809fa35fc19a
mahmudref@gmail.com <mahmudref@gmail.com>
2021-03-02 13:39:32 +0100

    exe2

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,12 @@
 function getAttributes() {
-
+var first = document.getElementById("pwcdr").href;
+alert('the value of href attribute of the link is : ' + first)
+var second = document.getElementById("pwcdr").hreflang;
+alert('the value of hreflang attribute of the link is : ' + second)
+var third = document.getElementById("pwcdr").rel;
+alert('the value of rel attribute of the link is : ' + third)
+var fourth = document.getElementById("pwcdr").target;
+alert('the value of target attribute of the link is : ' + fourth)
+var fifth = document.getElementById("pwcdr").type;
+alert('the value of type attribute of the link is : ' + fifth)
 }
