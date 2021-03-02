b32971b1df3a330ce0e0ae1d3784bf69f8768294
mahmudref@gmail.com <mahmudref@gmail.com>
2021-03-02 12:49:49 +0100

    exe1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,5 +1,14 @@
 // Use this function that is linked to the button to change the background
-
+ 
 function set_background() {
+    docBody = document.getElementsByTagName("body")[0];
+    myBodyElements = docBody.getElementsByTagName("p");
+    paragraph1 = myBodyElements[0];
+    paragraph1.style.background = "blue";
 
+    paragraph2 = myBodyElements[1];
+    paragraph2.style.background = "green";
+
+    paragraph3 = myBodyElements[2];
+    paragraph3.style.background = "yellow";
 }
